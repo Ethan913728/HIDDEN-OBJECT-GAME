76d4380a2bcc129d846c2ba5f85168ac6aab01a8
Ethan913728 <ethan.728.ortiz@gmail.com>
2025-04-02 20:12:22 +0000

    new file:   images/71KR9gNv9BL._AC_UF1000,1000_QL80_ (1).jpg 	new file:   images/bidoof-female-back.png 	modified:   index.html 	modified:   script.js 	modified:   styles.css

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let object = document.getElementById("object")
 let winMessage = document.getElementById("win")
 let restart = document.getElementById("restart")
+object.addEventListener("click", win)
+restart.addEventListener("click", reset)
 
+function win(){
+    object.setAttribute("class", "hidden")
+    winMessage.setAttribute("class", "")
+}
+
+function reset(){
+    let x = Math.random()*300
+    let y = Math.random()*300
+    object.style.top = x + "px"
+    object.style.left = y + "px"
+    object.setAttribute("class", "")
+    winMessage.setAttribute("class", "")
+}
 
 
 
